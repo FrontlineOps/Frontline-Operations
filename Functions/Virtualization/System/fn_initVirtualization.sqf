FrontlineOps/Frontline-Operations

commit 07e27099ce5d8d7e969de6d31640712eba184753

Refactor the rest of Virtualization & Bunch of fixes

Functions/Virtualization/System/fn_initVirtualization.sqf
@@ -1,0 +1,60 @@
+/*
+ * Function: FLO_fnc_initVirtualization
+ * Author: Frontline Operations Development Group
+ * Description:
+ *   Initializes the OPFOR virtualization system.
+ *   Creates HashMap for tracking virtualized groups.
+ *   Uses unscheduled CBA PerFrameHandler for reliable updates.
+ *
+ * Arguments:
+ * 0: Activation Distance <NUMBER> - Distance at which virtual groups activate (default 2000m)
+ *
+ * Return Value:
+ * Virtual Groups HashMap <HASHMAP>
+ *
+ * Example:
+ * [2000] call FLO_fnc_initVirtualization;
+ */
+
+params [["_activationDistance", 2000, [0]]];
+
+["VIRTUALIZATION", 3, format["Initializing Virtualization System (activation: %1m)", _activationDistance]] call FLO_fnc_log;
+
+// ============================================================================
+// CREATE MAIN DATA STRUCTURE
+// ============================================================================
+FLO_virtualGroups = createHashMapFromArray [
+    ["_groups", createHashMap],
+    ["_activationDistance", _activationDistance],
+    ["_enabled", true],
+    ["_debugMode", false]
+];
+
+// ============================================================================
+// INITIALIZE SUB-SYSTEMS
+// ============================================================================
+
+// Initialize spatial index for fast proximity queries
+[500] call FLO_fnc_virtualizationSpatialInit;
+
+// Register CBA event handlers for GTN/AI Commander integration
+["init"] call FLO_fnc_virtualizationEvents;
+
+// Start update loop (Unscheduled PFH)
+["start"] call FLO_fnc_virtualizationUpdatePFH;
+
+// Initialize debug manager
+["init"] call FLO_fnc_virtualizationDebugManager;
+
+// Initialize Transport System
+call FLO_fnc_transportConfig;
+call FLO_fnc_transportPool;
+call FLO_fnc_transportMapEdge;
+
+// Set ready flag
+FLO_VirtualizationReady = true;
+publicVariable "FLO_VirtualizationReady";
+
+["VIRTUALIZATION", 3, "Virtualization System initialized (PFH mode)"] call FLO_fnc_log;
+
+FLO_virtualGroups

Functions/Virtualization/System/fn_virtualizationEnsureUpdateState.sqf
@@ -1,0 +1,16 @@
+/*
+ * Function: FLO_fnc_virtualizationEnsureUpdateState
+ */
+
+params [
+    ["_batchSize", 25, [0]],
+    ["_playerCacheInterval", 1, [0]]
+];
+
+if (isNil "FLO_VirtUpdate") then {
+    FLO_VirtUpdate = [_batchSize, _playerCacheInterval] call FLO_fnc_virtualizationCreateUpdateState;
+};
+
+call FLO_fnc_virtualizationValidateUpdateState;
+
+FLO_VirtUpdate

Functions/Virtualization/System/fn_virtualizationUpdatePFH.sqf
@@ -1,0 +1,69 @@
+/*
+ * Function: FLO_fnc_virtualizationUpdatePFH
+ * Author: Frontline Operations Development Group
+ * Description:
+ *   Main virtualization update loop using CBA PerFrameHandler (unscheduled).
+ *
+ * Arguments:
+ * 0: Mode <STRING> - "start", "stop", "restart", "stats", "resetstats"
+ *
+ * Return Value:
+ * Boolean | HASHMAP
+ */
+
+params [["_mode", "start", [""]]];
+
+#define BATCH_SIZE            25
+#define PLAYER_CACHE_INTERVAL 1
+
+[BATCH_SIZE, PLAYER_CACHE_INTERVAL] call FLO_fnc_virtualizationEnsureUpdateState;
+
+switch (toLower _mode) do {
+    case "start": {
+        if (FLO_VirtUpdate get "running") exitWith {
+            ["VIRTUALIZATION", 3, "Update PFH already running"] call FLO_fnc_log;
+            true
+        };
+
+        private _pfhId = [{
+            if !(FLO_virtualGroups get "_enabled") exitWith {};
+            call FLO_fnc_virtualizationRunUpdateCycle;
+        }, 0, []] call CBA_fnc_addPerFrameHandler;
+
+        FLO_VirtUpdate set ["pfhId", _pfhId];
+        FLO_VirtUpdate set ["running", true];
+        FLO_VirtualGroupsUpdateLoopRunning = true;
+
+        ["VIRTUALIZATION", 3, "Update PFH started (unscheduled)"] call FLO_fnc_log;
+        true
+    };
+
+    case "stop": {
+        private _pfhId = FLO_VirtUpdate get "pfhId";
+        if (_pfhId >= 0) then {
+            [_pfhId] call CBA_fnc_removePerFrameHandler;
+            FLO_VirtUpdate set ["pfhId", -1];
+        };
+
+        FLO_VirtUpdate set ["running", false];
+        FLO_VirtualGroupsUpdateLoopRunning = false;
+
+        ["VIRTUALIZATION", 3, "Update PFH stopped"] call FLO_fnc_log;
+        true
+    };
+
+    case "restart": {
+        ["stop"] call FLO_fnc_virtualizationUpdatePFH;
+        ["start"] call FLO_fnc_virtualizationUpdatePFH;
+    };
+
+    case "stats": {
+        FLO_VirtUpdate get "stats"
+    };
+
+    case "resetstats": {
+        call FLO_fnc_virtualizationResetUpdateStats;
+    };
+
+    default { false };
+};

Functions/Virtualization/fn_initVirtualization.sqf
@@ -1,231 +1,0 @@
-/*
- * Function: FLO_fnc_initVirtualization
- * Author: Frontline Operations Development Group
- * Description:
- *   Initializes the OPFOR virtualization system.
- *   Creates HashMap for tracking virtualized groups.
- *   Uses unscheduled CBA PerFrameHandler for reliable updates.
- *
- * Arguments:
- * 0: Activation Distance <NUMBER> - Distance at which virtual groups activate (default 2000m)
- *
- * Return Value:
- * Virtual Groups HashMap <HASHMAP>
- *
- * Example:
- * [2000] call FLO_fnc_initVirtualization;
- */
-
-params [["_activationDistance", 2000, [0]]];
-
-["VIRTUALIZATION", 3, format["Initializing Virtualization System (activation: %1m)", _activationDistance]] call FLO_fnc_log;
-
-// ============================================================================
-// CREATE MAIN DATA STRUCTURE
-// ============================================================================
-FLO_virtualGroups = createHashMapObject [[
-    // Data storage
-    ["_groups", createHashMap],
-    ["_activationDistance", _activationDistance],
-    ["_enabled", true],
-    ["_debugMode", false],  // Managed by FLO_fnc_virtualizationDebugManager
-
-    // Enable/disable the virtualization system
-    ["_setEnabled", {
-        params ["_self", "_enable"];
-        _self set ["_enabled", _enable];
-
-        if (_enable) then {
-            ["start"] call FLO_fnc_virtualizationUpdatePFH;
-        } else {
-            ["stop"] call FLO_fnc_virtualizationUpdatePFH;
-        };
-
-        ["VIRTUALIZATION", 3, format["Virtualization %1", if (_enable) then {"enabled"} else {"disabled"}]] call FLO_fnc_log;
-    }],
-
-    // Get a specific group
-    ["_getGroup", {
-        params ["_self", "_groupId"];
-        (_self get "_groups") getOrDefault [_groupId, nil]
-    }],
-
-    // Add a group to the system
-    ["_addGroup", {
-        params ["_self", "_groupId", "_groupData"];
-        (_self get "_groups") set [_groupId, _groupData];
-
-        // Add to spatial index - position must exist
-        private _pos = _groupData get "position";
-        ["add", [_groupId, _pos, _groupData get "side"]] call FLO_fnc_virtualizationSpatialIndex;
-        if ([_groupData] call FLO_fnc_gtnCombatAffectsClassification) then {
-            [true] call FLO_fnc_gtnCombatMarkClassificationDirty;
-        };
-
-        // Fire event for other systems (GTN, AI Commander)
-        ["FLO_Virtualization_GroupAdded", [_groupId, _groupData]] call CBA_fnc_localEvent;
-    }],
-
-    // Remove a group from the system
-    ["_removeGroup", {
-        params ["_self", "_groupId"];
-        private _groups = _self get "_groups";
-        private _groupData = _groups getOrDefault [_groupId, nil];
-
-        if (!isNil "_groupData") then {
-            // Remove from spatial index
-            ["remove", [_groupId]] call FLO_fnc_virtualizationSpatialIndex;
-            if ([_groupData] call FLO_fnc_gtnCombatAffectsClassification) then {
-                [true] call FLO_fnc_gtnCombatMarkClassificationDirty;
-            };
-
-            // Clean up debug markers
-            ["cleanup", _groupId] call FLO_fnc_virtualizationDebugManager;
-
-            // Remove from storage
-            _groups deleteAt _groupId;
-
-            // Fire event for other systems
-            ["FLO_Virtualization_GroupRemoved", [_groupId]] call CBA_fnc_localEvent;
-
-            ["VIRTUALIZATION", 4, format["Removed group %1", _groupId]] call FLO_fnc_log;
-        };
-    }],
-
-    // Update position of a virtual group
-    ["_updateGroupPosition", {
-        params ["_self", "_groupId", "_newPosition"];
-        private _groupData = (_self get "_groups") getOrDefault [_groupId, nil];
-
-        if (!isNil "_groupData") then {
-            // Validate position
-            if ((_newPosition select 0) > 100 || (_newPosition select 1) > 100) then {
-                private _oldPosition = _groupData get "position";
-                private _groupType = _groupData get "groupType";
-                private _side = _groupData get "side";
-                private _trackCombatSeed = (_side in [east, west]) && {([_groupData] call FLO_fnc_virtualizationGetTransportAttachment) == ""} && {_groupType in ["infantry", "motorized", "mechanized", "armor", "mobile_aa"]};
-                private _seedCellSize = if (isNil "FLO_GTN_CombatState") then { 150 } else { FLO_GTN_CombatState get "classificationSeedCellSize" };
-
-                _groupData set ["position", _newPosition];
-
-                // Update spatial index (only if position changed significantly)
-                ["update", [_groupId, _newPosition, _groupData get "side"]] call FLO_fnc_virtualizationSpatialIndex;
-
-                if (_trackCombatSeed) then {
-                    private _oldSeedCellKey = format [
-                        "%1_%2",
-                        floor ((_oldPosition select 0) / _seedCellSize),
-                        floor ((_oldPosition select 1) / _seedCellSize)
-                    ];
-                    private _newSeedCellKey = format [
-                        "%1_%2",
-                        floor ((_newPosition select 0) / _seedCellSize),
-                        floor ((_newPosition select 1) / _seedCellSize)
-                    ];
-                    if (_oldSeedCellKey != _newSeedCellKey) then {
-                        [false] call FLO_fnc_gtnCombatMarkClassificationDirty;
-                    };
-                };
-            };
-        };
-    }],
-
-    // Reserve a group for a mission (prevents deactivation)
-    ["_reserveGroup", {
-        params ["_self", "_groupId", ["_missionType", "unknown"]];
-        private _groupData = (_self get "_groups") getOrDefault [_groupId, nil];
-
-        if (!isNil "_groupData") then {
-            [_groupData, "VIRTUALIZATION", _missionType] call FLO_fnc_virtualizationSetMissionLock;
-            [_groupData, "reserved"] call FLO_fnc_virtualizationSetRuntimeState;
-            if (
-                (_groupData get "side") in [east, west]
-                && {([_groupData] call FLO_fnc_virtualizationGetTransportAttachment) == ""}
-                && {[(_groupData get "groupType")] call FLO_fnc_gtnCombatIsSupportProvider}
-            ) then {
-                [false] call FLO_fnc_gtnCombatMarkClassificationDirty;
-            };
-
-            ["FLO_Virtualization_GroupReserved", [_groupId, _missionType]] call CBA_fnc_localEvent;
-            true
-        } else {
-            false
-        };
-    }],
-
-    // Release a group from mission (allows deactivation again)
-    ["_releaseGroup", {
-        params ["_self", "_groupId"];
-        private _groupData = (_self get "_groups") getOrDefault [_groupId, nil];
-
-        if (!isNil "_groupData") then {
-            [_groupData] call FLO_fnc_virtualizationClearMissionLock;
-            [_groupData, "idle"] call FLO_fnc_virtualizationSetRuntimeState;
-            if (
-                (_groupData get "side") in [east, west]
-                && {([_groupData] call FLO_fnc_virtualizationGetTransportAttachment) == ""}
-                && {[(_groupData get "groupType")] call FLO_fnc_gtnCombatIsSupportProvider}
-            ) then {
-                [false] call FLO_fnc_gtnCombatMarkClassificationDirty;
-            };
-
-            ["FLO_Virtualization_GroupReleased", [_groupId]] call CBA_fnc_localEvent;
-            true
-        } else {
-            false
-        };
-    }],
-
-    // Get groups by criteria
-    ["_getGroupsBy", {
-        params ["_self", "_criteria"];
-        private _groups = _self get "_groups";
-        private _result = [];
-
-        {
-            private _match = true;
-            {
-                private _key = _x;
-                private _val = _criteria get _key;
-                if ((_y getOrDefault [_key, nil]) isNotEqualTo _val) then {
-                    _match = false;
-                };
-            } forEach _criteria;
-
-            if (_match) then {
-                _result pushBack [_x, _y];
-            };
-        } forEach _groups;
-
-        _result
-    }]
-]];
-
-// ============================================================================
-// INITIALIZE SUB-SYSTEMS
-// ============================================================================
-
-// Initialize spatial index for fast proximity queries
-["init", [500]] call FLO_fnc_virtualizationSpatialIndex;
-
-// Register CBA event handlers for GTN/AI Commander integration
-["init"] call FLO_fnc_virtualizationEvents;
-
-// Start update loop (Unscheduled PFH)
-["start"] call FLO_fnc_virtualizationUpdatePFH;
-
-// Initialize debug manager
-["init"] call FLO_fnc_virtualizationDebugManager;
-
-// Initialize Transport System
-call FLO_fnc_transportConfig;
-call FLO_fnc_transportPool;
-call FLO_fnc_transportMapEdge;
-
-// Set ready flag
-FLO_VirtualizationReady = true;
-publicVariable "FLO_VirtualizationReady";
-
-["VIRTUALIZATION", 3, "Virtualization System initialized (PFH mode)"] call FLO_fnc_log;
-
-FLO_virtualGroups

commit e973a87e4b98093a4353606b9b67cbce97a13776

Fix Critical FPS Bug introduced in 07e27099ce5d8d7e969de6d31640712eba184753

Functions/Virtualization/System/fn_initVirtualization.sqf
@@ -45,8 +45,8 @@ FLO_virtualGroups = createHashMapFromArray [
 // Initialize spatial index for fast proximity queries
 [500] call FLO_fnc_virtualizationSpatialInit;
 
-// Initialize shared update/budget state before any pre-PFH activations occur.
-call FLO_fnc_virtualizationEnsureUpdateState;
+// Rebuild ephemeral update state for each mission init
+[25, 1, true] call FLO_fnc_virtualizationEnsureUpdateState;
 
 // Register CBA event handlers for GTN/AI Commander integration
 ["init"] call FLO_fnc_virtualizationEvents;

Functions/Virtualization/System/fn_virtualizationEnsureUpdateState.sqf
@@ -4,11 +4,35 @@
 
 params [
     ["_batchSize", 25, [0]],
-    ["_playerCacheInterval", 1, [0]]
+    ["_playerCacheInterval", 1, [0]],
+    ["_forceReset", false, [true]]
 ];
 
-if (isNil "FLO_VirtUpdate") then {
+if (isNil "FLO_VirtUpdate" || {_forceReset}) then {
     FLO_VirtUpdate = [_batchSize, _playerCacheInterval] call FLO_fnc_virtualizationCreateUpdateState;
+} else {
+    if ((FLO_VirtUpdate get "batchSize") != _batchSize) then {
+        ["VIRTUALIZATION", 2, format [
+            "Correcting stale virtualization batch size %1 -> %2",
+            FLO_VirtUpdate get "batchSize",
+            _batchSize
+        ]] call FLO_fnc_log;
+        FLO_VirtUpdate set ["batchSize", _batchSize];
+        FLO_VirtUpdate set ["currentBatchIndex", 0];
+        FLO_VirtUpdate set ["cachedGroupIds", []];
+        FLO_VirtUpdate set ["lastGroupCacheTime", 0];
+    };
+
+    if ((FLO_VirtUpdate get "playerCacheInterval") != _playerCacheInterval) then {
+        ["VIRTUALIZATION", 2, format [
+            "Correcting stale player cache interval %1 -> %2",
+            FLO_VirtUpdate get "playerCacheInterval",
+            _playerCacheInterval
+        ]] call FLO_fnc_log;
+        FLO_VirtUpdate set ["playerCacheInterval", _playerCacheInterval];
+        FLO_VirtUpdate set ["cachedPlayerPositions", []];
+        FLO_VirtUpdate set ["lastPlayerCacheTime", 0];
+    };
 };
 
 call FLO_fnc_virtualizationValidateUpdateState;

Functions/Virtualization/System/fn_virtualizationUpdatePFH.sqf
@@ -16,7 +16,30 @@ params [["_mode", "start", [""]]];
 #define BATCH_SIZE            25
 #define PLAYER_CACHE_INTERVAL 1
 
-[BATCH_SIZE, PLAYER_CACHE_INTERVAL] call FLO_fnc_virtualizationEnsureUpdateState;
+private _updateState = [BATCH_SIZE, PLAYER_CACHE_INTERVAL] call FLO_fnc_virtualizationEnsureUpdateState;
+
+if ((_updateState get "batchSize") != BATCH_SIZE) then {
+    ["VIRTUALIZATION", 2, format [
+        "Correcting stale virtualization batch size %1 -> %2 before PFH start",
+        _updateState get "batchSize",
+        BATCH_SIZE
+    ]] call FLO_fnc_log;
+    _updateState set ["batchSize", BATCH_SIZE];
+    _updateState set ["currentBatchIndex", 0];
+    _updateState set ["cachedGroupIds", []];
+    _updateState set ["lastGroupCacheTime", 0];
+};
+
+if ((_updateState get "playerCacheInterval") != PLAYER_CACHE_INTERVAL) then {
+    ["VIRTUALIZATION", 2, format [
+        "Correcting stale player cache interval %1 -> %2 before PFH start",
+        _updateState get "playerCacheInterval",
+        PLAYER_CACHE_INTERVAL
+    ]] call FLO_fnc_log;
+    _updateState set ["playerCacheInterval", PLAYER_CACHE_INTERVAL];
+    _updateState set ["cachedPlayerPositions", []];
+    _updateState set ["lastPlayerCacheTime", 0];
+};
 
 switch (toLower _mode) do {
     case "start": {